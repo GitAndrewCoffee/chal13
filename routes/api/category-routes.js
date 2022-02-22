b0a649f9939728e798614625ebea2770200f4e05
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-02-21 20:40:00 -0600

    updated models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,16 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.create({
+    title: req.body.title,
+    notes: req.body.notes,
+    event_id: req.body.event_id,
+})
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.get('/:id', (req, res) => {
