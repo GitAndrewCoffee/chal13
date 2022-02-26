d7a0d825aeabf7eda3a480264fb2010bc11da88f
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-02-26 09:08:40 -0600

    final submission

--- models/index.js
+++ models/index.js
@@ -16,21 +16,23 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id'
-});
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Product, {
+//   through: {ProductTag,
+//     foreignKey: 'tag_id'
+//     },
+//   as: 'whatchamacallit'
+// });
 
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'products_with_tag'
-});
+// Tag.belongsToMany(Product, {
+//   through: {
+//     model: ProductTag,
+//     unique: false
+//   },
+//   as: 'products_with_tag'
+// });
 
 module.exports = {
   Product,
